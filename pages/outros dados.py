7cc4c61669b3bb3244040b190c98cea5135fb65f
Luis Guilherme <111072200+LGuilher2003@users.noreply.github.com>
2024-06-04 19:35:56 -0300

    Update outros dados.py

--- pages/outros dados.py
+++ pages/outros dados.py
@@ -14,10 +14,21 @@ with open('style.css') as f:
 idade_counts = df['Idade -Cálculo média'].value_counts().reset_index()
 idade_counts.columns = ['Idade', 'Count']
 
-fig = px.pie(idade_counts, values='Count', names='Idade', title='Média Total das Idades',color_discrete_map={'11': 'pink', '12': 'blue','13':''})
+fig = px.pie(idade_counts, values='Count', names='Idade', title='Média Total das Idades')
 fig.update_traces(
     textposition='inside',
     textinfo='percent+label'
 )
 st.plotly_chart(fig, use_container_width=True)
+
+color_discrete_map = {'M': 'Blue', 'F': 'pink'}
+fig_sexo = px.histogram(df, x='Turma',nbins=30,color='Sexo',title='Distribuição do sexo por turma',
+                        color_discrete_map=color_discrete_map)
+fig_sexo.update_layout(
+    plot_bgcolor='white',
+    xaxis_title='Sexo',
+    yaxis_title='Frequência',
+    bargap=0.1 
+)
+st.plotly_chart(fig_sexo, use_container_width=True)
 
